2e2562282fbc6133e1e0c3b0ea2e77b6012d5985
kallgren <robertkallgren@hotmail.com>
2014-07-05 23:23:31 +0200

    Improved connection experience

--- app/src/main/java/com/robertkallgren/kgnchat/ConnectActivity.java
+++ app/src/main/java/com/robertkallgren/kgnchat/ConnectActivity.java
@@ -3,6 +3,9 @@ package com.robertkallgren.kgnchat;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,6 +13,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import java.io.BufferedReader;
@@ -17,6 +21,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -112,12 +117,29 @@ public class ConnectActivity extends Activity {
         if (cancel) {
             focusView.requestFocus();
         } else {
+            hideSoftKeyboard();
             showProgress(true);
-            connectTask = new ConnectTask(nick, address, port);
+            connectTask = new ConnectTask(nick, address, port, 5000);
             connectTask.execute((Void) null);
         }
     }
 
+    /**
+     * Hides the soft keyboard.
+     */
+    private void hideSoftKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+
+        View currentView = getCurrentFocus();
+        if (currentView != null) {
+            imm.hideSoftInputFromWindow(currentView.getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * Shows the progress UI and hides the connection form.
+     */
     private void showProgress(final boolean show) {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -151,11 +173,13 @@ public class ConnectActivity extends Activity {
         private final String nick;
         private final String address;
         private final int port;
+        private final int timeout;
 
-        ConnectTask(String nick, String address, int port) {
+        ConnectTask(String nick, String address, int port, int timeout) {
             this.nick = nick;
             this.address = address;
             this.port = port;
+            this.timeout = timeout;
         }
 
         @Override
@@ -165,7 +189,8 @@ public class ConnectActivity extends Activity {
                 InetAddress inetAddress = InetAddress.getByName(address);
 
                 Log.d(TAG, "Creating socket to " + inetAddress.getHostAddress() + ":" + port);
-                Socket connection = new Socket(inetAddress, port);
+                Socket connection = new Socket();
+                connection.connect(new InetSocketAddress(inetAddress, port), timeout);
                 DataOutputStream out = new DataOutputStream(connection.getOutputStream());
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(connection.getInputStream()));
@@ -197,20 +222,40 @@ public class ConnectActivity extends Activity {
 
             switch (result) {
                 case NETWORK_ERROR:
+                    displayConnectionFailedAlert();
                     addressEditText.setError(getString(R.string.error_connection_failed));
-                    addressEditText.requestFocus();
-                    Log.d(TAG, "Connection failed, displaying 'network error' message");
+                    portEditText.setError(getString(R.string.error_connection_failed));
+                    Log.d(TAG, "Connection failed, displaying alert dialog");
                     break;
                 case NICK_IN_USE:
                     nickEditText.setError(getString(R.string.error_nick_in_use));
                     nickEditText.requestFocus();
-                    Log.d(TAG, "Connection failed, displaying 'nick in use' message");
+                    Log.d(TAG, "Connection failed, displaying 'nick in use' error");
                     break;
                 default:
                     Log.d(TAG, "Connection succeeded, launching chat activity");
                     // TODO: Launch chat activity.
                     // finish();
             }
+        }
+
+        /**
+         * Displays a 'Connection failed' alert.
+         */
+        private void displayConnectionFailedAlert() {
+            String message = getString(R.string.error_connection_failed) + ". " +
+                    getString(R.string.error_connection_failed_tip) + ".";
+
+            new AlertDialog.Builder(ConnectActivity.this)
+                    .setMessage(message)
+                    .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            connect(null);
+                        }
+                    })
+                    .setNegativeButton(R.string.close, null)
+                    .show();
         }
 
         @Override
